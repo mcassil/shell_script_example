d3b5b7c6afae5691cda6c4f0ed3d22b29b4f2dd6
mcassil <68384457+mcassil@users.noreply.github.com>
2024-05-22 09:54:19 -0300

    Comandos em variaveis.sh

---
+++ comandos em variaveis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Maciel Castro
+# e-mail: macielcastro@rocketmail.com
+var1=6
+var2=5
+var3=4
+var4=`bc << EOF
+scale=2
+a1=($var1 * $var2)
+b1=($var3 + $var1)
+a1+b2
+EOF
+`
+echo "O resultado do calculo é $var4"
