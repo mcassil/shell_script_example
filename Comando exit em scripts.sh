62c25b62cab7b25b4cbf7a011d3d58aaae9d634a
mcassil <68384457+mcassil@users.noreply.github.com>
2024-05-22 11:42:58 -0300

    Create Comando exit em scripts.sh

---
+++ Comando exit em scripts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Maciel Castro
+# e-mail: macielcastro@rocketmail.com
+var1=2
+var2=2
+var3=$[$var1 + $var2]
+echo "Resultado: $var3"
+exit $var3
+# Digite "echo $?" para ver o status de saida do script.
