67e32a35eed0b7c90ef87b8cdf56d35e7207e841
mcassil <68384457+mcassil@users.noreply.github.com>
2024-05-22 15:39:08 -0300

    Create Comparações numericas e de strings com comando test-String2.sh

---
+++ Comparações numericas e de strings com comando test-String2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Maciel Castro
+# e-mail: macielcastro@rocketmail.com
+fruta=laranjaa
+if [ $fruta != laranja ]
+then
+    echo "A fruta não é uma laranja, é $fruta"
+else
+    echo "A fruta é $fruta"
+fi
